424a817f5e344b3d503da4372bcb941010d0d6ce
MD SAYDUR RAHMAN <93133412+engrsayed90@users.noreply.github.com>
2023-02-15 22:32:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let msg1 = document.getElementById("message1");
-var msg2 = document.getElementById("message2");
+let msg2 = document.getElementById("message2");
 var msg3 = document.getElementById("message3");
 
 var answer = Math.floor(Math.random()*100) + 1;
