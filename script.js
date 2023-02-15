a2c8243beba7509a7ed8af26d679cef7ebabf180
MD SAYDUR RAHMAN <93133412+engrsayed90@users.noreply.github.com>
2023-02-15 22:32:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var msg1 = document.getElementById("message1");
+let msg1 = document.getElementById("message1");
 var msg2 = document.getElementById("message2");
 var msg3 = document.getElementById("message3");
 
